e86be120393d4e5f2cafcc865c18967d2bfc1b79
vanyauhalin <vanyauhalin@gmail.com>
2022-04-30 19:19:30 +0300

    chore: add esbuild

---
+++ .esbuildrc.js
@@ -0,0 +1,13 @@
+import { resolve } from 'path';
+import { buildSync } from 'esbuild';
+
+const result = buildSync({
+  allowOverwrite: true,
+  bundle: true,
+  entryPoints: [resolve('src/scripts/main.ts')],
+  outfile: resolve('lib/psfy.js'),
+  platform: 'browser',
+});
+if (result.errors.length > 0) {
+  throw new Error(result.errors);
+}
