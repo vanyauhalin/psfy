9cc459842723e7c0dab7a7260054d418c00500da
vanyauhalin <vanyauhalin@gmail.com>
2022-05-05 23:33:42 +0300

    refactor: css vars

--- src/scripts/expanded.ts
+++ src/scripts/expanded.ts
@@ -1,15 +1,13 @@
 import { debounce, extract, wait } from './utils';
 
-const NAVBAR_MAX_WIDTH = '--psfy-navbar-max-width';
-const NAVBAR_MIN_WIDTH = '--psfy-navbar-min-width';
 const NAVBAR_WIDTH = '--nav-bar-width';
 
 wait(() => document.querySelector('.LayoutResizer__input')).then((el) => {
   function parse(value: ReturnType<typeof extract>): number {
     return parseInt(value as string, 10);
   }
-  const maxWidth = parse(extract(NAVBAR_MAX_WIDTH));
-  const minWidth = parse(extract(NAVBAR_MIN_WIDTH));
+  const maxWidth = parse(extract('--psfy-navbar-max-wd'));
+  const minWidth = parse(extract('--psfy-navbar-min-wd'));
   const correct = debounce(() => {
     const width = parse(extract.force(NAVBAR_WIDTH));
     if (width >= minWidth && width <= maxWidth) return;
